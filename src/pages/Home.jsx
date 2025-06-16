f2e79fef20058eedfea1b88b54ad0e3adf59ef9b
EmyBrown <emybrown620@gmail.com>
2025-06-16 01:23:38 +0000

    `Refactored Home page JSX with updated styles and layout changes`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,9 +45,9 @@ function Home() {
       <section  className=' px-[24px] md:px-[39px] lg:px-[165px]'>
         <div className='py-[29%] md:py-[22%] lg:py-[12%] flex justify-center lg:justify-start'>
         <div className='   flex flex-col text-center  items-center lg:items-start lg:text-left '>
-          <p className='text-[#979797] text-sm mb-4 tracking-[10px]'>NEW PRODUCT</p>
-          <p className='text-gray-50  text-5xl md:text-6xl lg:w-[50%] md:w-[55%] mb-5'>XX99 MARK II HEADPHONES</p>
-          <p className='text-[#979797] font-medium text-lg lg:[50%] md:w-[55%] mb-5'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+          <p className='text-[#979797] text-sm  mb-4 tracking-[10px]'>NEW PRODUCT</p>
+          <p className='text-gray-50  text-5xl md:text-6xl lg:w-[50%] md:w-[55%] 2xl:text-8xl 2xl:mb-8 mb-5'>XX99 MARK II HEADPHONES</p>
+          <p className='text-[#979797] font-medium text-lg lg:[50%] md:w-[55%] 2xl:text-xl 2xl:mb-8 mb-5'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
           <button className='bg-[#D87D4A] font-bold text-gray-50 py-2.5 lg:py-4 px-8'>SEE PRODUCT</button>
         </div>
         </div>
@@ -68,18 +68,19 @@ function Home() {
       </div>
 
 
-      <section className='relative '>
-      <div className='bg-[#D87D4A] rounded-lg  w-full lg:h-[30rem] 2xl:h-[40.5rem]  flex justify-center items-center lg:items-start  absolute  -z-4'>
+      <section className='relative cursor-default'>
+      <div className='bg-[#D87D4A] rounded-lg  w-full lg:h-[32rem] 2xl:h-[39.5rem]  flex justify-center items-center lg:items-start  absolute  -z-4'>
         <img src={circlePattern} className=' absolute -z-5 top-[-1rem] lg:top-[-2rem] lg:left-[-15rem]' alt="" />
         
-        <div className='lg:flex mt-[5rem] lg:mt-[7rem] text-gray-50 '>
-        <img src={speakerThumbnail} className=' z-10 w-4xl 2xl:w-4xl ' alt="" />
-        <div className='border-2 border-red-600 h-[0%] flex flex-col justify-center items-center text-center lg:items-start text-start'>
-          <h1 className='font-bold  border-2 text-5xl 2xl:text-6xl w-1/2 lg:mt-8 2xl:mt-15 2xl:mb-8 mb-4 text-center'>ZX9 SPEAKER</h1>
-          <p className='w-[14rem] lg:w-2/3 2xl:w-1/2 2xl:mb-8 2xl:text-lg mb-4 border-2'>
+        {/* <div className="flex flex-col lg:flex-row items-center lg:items-start mt-[5rem] lg:mt-[7rem] text-white gap-8 px-6 text-center lg:text-left"> */}
+        <div className='flex flex-col lg:flex-row items-center lg:items-start mt-[5rem] lg:mt-[4rem] gap-8 px-6 text-gray-50 text-center lg:text-left '>
+        <img src={speakerThumbnail} className=' lg:ml-[-5rem] 2xl:ml-[-15rem] z-10 w-4xl 2xl:w-4xl ' alt="" />
+        <div className=' h-[0%] max-w-[22rem] lg:max-w-[24rem] lg:ml-[-5rem] ' >
+          <p className='font-bold text-5xl 2xl:text-6xl lg:w-1/2 lg:mt-8 2xl:mt-15 2xl:mb-8 mb-4 '>ZX9 SPEAKER</p>
+          <p className=' lg:w-full 2xl:mb-8 2xl:text-lg mb-4 '>
             Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
           </p>
-          <button className='bg-[#000000] font-bold text-gray-50 py-2.5 2xl:py-4 px-8'>SEE PRODUCT</button>
+          <button className='hover:bg-[#979797] cursor-pointer bg-[#000000] font-bold text-gray-50 py-2.5 2xl:py-4 px-8 mb-[4rem]'>SEE PRODUCT</button>
           
         </div>
         </div>
