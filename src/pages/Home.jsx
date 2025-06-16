c2629057d750e7bb54be8b580e66305c1898f714
EmyBrown <emybrown620@gmail.com>
2025-06-16 00:13:16 +0000

    `Refactor Home page layout and content`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
       <section  className=' px-[24px] md:px-[39px] lg:px-[165px]'>
         <div className='py-[29%] md:py-[22%] lg:py-[12%] flex justify-center lg:justify-start'>
         <div className='   flex flex-col text-center  items-center lg:items-start lg:text-left '>
-          <p className='text-[#979797] text-xl mb-4'>NEW PRODUCT</p>
+          <p className='text-[#979797] text-sm mb-4 tracking-[10px]'>NEW PRODUCT</p>
           <p className='text-gray-50  text-5xl md:text-6xl lg:w-[50%] md:w-[55%] mb-5'>XX99 MARK II HEADPHONES</p>
           <p className='text-[#979797] font-medium text-lg lg:[50%] md:w-[55%] mb-5'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
           <button className='bg-[#D87D4A] font-bold text-gray-50 py-2.5 lg:py-4 px-8'>SEE PRODUCT</button>
@@ -66,13 +66,26 @@ function Home() {
         </div>
       ))}
       </div>
-      <div className='relative '>
-      <div className='bg-[#D87D4A] rounded-lg  w-full lg:h-[30rem] 2xl:h-[40rem]  lg:items-start  absolute  -z-4'>
-        <img src={circlePattern} className=' absolute -z-5 top-[-2rem] lg:left-[-15rem]' alt="" />
-        <img src={speakerThumbnail} className=' z-10 pb[-8rem]  border-2 mt-[9rem] w-1/2 max-w-3xl ' alt="" />
+
+
+      <section className='relative '>
+      <div className='bg-[#D87D4A] rounded-lg  w-full lg:h-[30rem] 2xl:h-[40.5rem]  flex justify-center items-center lg:items-start  absolute  -z-4'>
+        <img src={circlePattern} className=' absolute -z-5 top-[-1rem] lg:top-[-2rem] lg:left-[-15rem]' alt="" />
+        
+        <div className='lg:flex mt-[5rem] lg:mt-[7rem] text-gray-50 '>
+        <img src={speakerThumbnail} className=' z-10 w-4xl 2xl:w-4xl ' alt="" />
+        <div className='border-2 border-red-600 h-[0%] flex flex-col justify-center items-center text-center lg:items-start text-start'>
+          <h1 className='font-bold  border-2 text-5xl 2xl:text-6xl w-1/2 lg:mt-8 2xl:mt-15 2xl:mb-8 mb-4 text-center'>ZX9 SPEAKER</h1>
+          <p className='w-[14rem] lg:w-2/3 2xl:w-1/2 2xl:mb-8 2xl:text-lg mb-4 border-2'>
+            Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+          </p>
+          <button className='bg-[#000000] font-bold text-gray-50 py-2.5 2xl:py-4 px-8'>SEE PRODUCT</button>
+          
+        </div>
+        </div>
         </div>
 
-      </div>
+      </section>
 
       </section>
       
